775ba569f047673c81972cba7e6b58f256502c3b
Aphaphia <St10480706@rcconnect.edu.za>
2026-06-26 14:22:28 +0200

    Add files via upload

---
+++ part3/part3/VoiceGreeting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Media;
+
+namespace CyberSecurityPart3
+{
+    public static class VoiceGreeting
+    {
+        public static void Play()
+        {
+            PlayGreeting("greeting.wav");
+        }
+
+        public static void PlayGreeting(string filepath)
+        {
+            string fullPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, filepath);
+            if (File.Exists(fullPath))
+            {
+                SoundPlayer player = new SoundPlayer(fullPath);
+                player.Play();
+            }
+        }
+    }
+}s
